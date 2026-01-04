9cdc2f8952753a50a15cea568f9085207d9ea183
Rashath A <146910342+Rashath23@users.noreply.github.com>
2026-01-04 13:24:34 +0530

    Add LocalSum class to calculate sum of two numbers

---
+++ local.java
@@ -0,0 +1,10 @@
+public class LocalSum {
+    public static void main(String[] args) {
+
+        int a = 10;   // local variable
+        int b = 20;   // local variable
+
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+}
