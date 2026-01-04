63c0bd583a820cd66fdf0f3ffeace47702ee8e32
Rashath A <146910342+Rashath23@users.noreply.github.com>
2026-01-04 13:25:37 +0530

    Add StaticAccess class with static variable example

---
+++ static.java
@@ -0,0 +1,8 @@
+public class StaticAccess {
+
+    static int count = 50;   // static variable
+
+    public static void main(String[] args) {
+        System.out.println("Count = " + StaticAccess.count);
+    }
+}
