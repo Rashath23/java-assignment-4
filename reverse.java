7b6d49191790bb76331e3849d071b3e9ec664bd1
Rashath A <146910342+Rashath23@users.noreply.github.com>
2026-01-22 13:43:27 +0530

    Implement left rotation of an array using reversal

---
+++ reverse.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class LeftRotateReversal {
+    static void reverse(int arr[], int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    static void leftRotate(int arr[], int n, int k) {
+        k = k % n;
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+        reverse(arr, 0, n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int k = sc.nextInt();
+
+        leftRotate(arr, n, k);
+
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
